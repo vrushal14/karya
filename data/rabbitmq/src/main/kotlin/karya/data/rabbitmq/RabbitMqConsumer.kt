c6d75067773643c3206949680da5fed116ec0b00
Saumya Bhatt <saumya.bhatt106@gmail.com>
2024-11-22 21:00:20 +0530

    Code review

---
+++ data/rabbitmq/src/main/kotlin/karya/data/rabbitmq/RabbitMqConsumer.kt
@@ -0,0 +1,48 @@
+package karya.data.rabbitmq
+
+import com.rabbitmq.client.AMQP
+import com.rabbitmq.client.Channel
+import com.rabbitmq.client.DefaultConsumer
+import com.rabbitmq.client.Envelope
+import karya.core.queues.entities.ExecutorMessage
+import karya.data.rabbitmq.RabbitMqQueueClient.Companion.json
+import kotlinx.coroutines.runBlocking
+import org.apache.logging.log4j.kotlin.Logging
+import javax.inject.Inject
+
+class RabbitMqConsumer
+	@Inject
+	constructor(
+		private val channel: Channel,
+	) : DefaultConsumer(channel) {
+		companion object : Logging
+
+		var onMessage: (suspend (ExecutorMessage) -> Unit)? = null
+
+		override fun handleDelivery(
+			consumerTag: String?,
+			envelope: Envelope?,
+			properties: AMQP.BasicProperties?,
+			body: ByteArray?,
+		) {
+			envelope?.let {
+				try {
+					val message = parseMessage(body)
+					onMessage?.let { handler ->
+						runBlocking { handler(message) } // Process the message
+					}
+					channel.basicAck(envelope.deliveryTag, false)
+				} catch (e: Exception) {
+					channel.basicNack(envelope.deliveryTag, false, true) // Requeue message on failure
+					logger.error("Error processing message: ${e.message}", e)
+				}
+			} ?: logger.error("Received null envelope, message discarded.")
+		}
+
+		private fun parseMessage(body: ByteArray?): ExecutorMessage {
+			val messageJson =
+				body?.toString(Charsets.UTF_8)
+					?: throw IllegalArgumentException("Message body is null")
+			return json.decodeFromString<ExecutorMessage>(messageJson)
+		}
+	}
--- data/rabbitmq/src/main/kotlin/karya/data/rabbitmq/RabbitMqQueueClient.kt
+++ data/rabbitmq/src/main/kotlin/karya/data/rabbitmq/RabbitMqQueueClient.kt
@@ -3,17 +3,15 @@ package karya.data.rabbitmq
 import com.rabbitmq.client.AMQP.BasicProperties
 import com.rabbitmq.client.Channel
 import com.rabbitmq.client.Connection
-import com.rabbitmq.client.DefaultConsumer
-import com.rabbitmq.client.Envelope
 import karya.core.queues.QueueClient
 import karya.core.queues.entities.ExecutorMessage
-import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.json.Json
 import org.apache.logging.log4j.kotlin.Logging
 
 class RabbitMqQueueClient(
 	private val channel: Channel,
 	private val connection: Connection,
+	private val consumer: RabbitMqConsumer,
 ) : QueueClient {
 	companion object : Logging {
 		private const val EXCHANGE_NAME = "karya-exchange"
@@ -23,7 +21,7 @@ class RabbitMqQueueClient(
 		private const val DELIVERY_MODE = 2 // persistent
 		private const val PRIORITY_LEVEL = 1
 
-		private val json =
+		val json =
 			Json {
 				ignoreUnknownKeys = true
 				isLenient = true
@@ -59,21 +57,9 @@ class RabbitMqQueueClient(
 		}
 	}
 
+	// will maintain a persistent connection so no need for polling
 	override suspend fun consume(onMessage: suspend (ExecutorMessage) -> Unit) {
-		val consumer =
-			object : DefaultConsumer(channel) {
-				override fun handleDelivery(
-					consumerTag: String?,
-					envelope: Envelope?,
-					properties: BasicProperties?,
-					body: ByteArray?,
-				) {
-					envelope?.let {
-						processMessage(it, body, onMessage)
-					} ?: logger.error("Received null envelope, message discarded.")
-				}
-			}
-
+		consumer.onMessage = onMessage
 		channel.basicConsume(EXECUTOR_QUEUE_NAME, false, consumer)
 		logger.info("Started consuming messages from queue: $EXECUTOR_QUEUE_NAME")
 	}
@@ -101,21 +87,4 @@ class RabbitMqQueueClient(
 			.deliveryMode(DELIVERY_MODE)
 			.priority(PRIORITY_LEVEL)
 			.build()
-
-	private fun processMessage(
-		envelope: Envelope,
-		body: ByteArray?,
-		onMessage: suspend (ExecutorMessage) -> Unit,
-	) {
-		try {
-			val messageJson = body?.toString(Charsets.UTF_8) ?: throw IllegalArgumentException("Message body is null")
-			val message = json.decodeFromString<ExecutorMessage>(messageJson)
-
-			runBlocking { onMessage(message) } // Call the provided lambda
-			channel.basicAck(envelope.deliveryTag, false) // Acknowledge success
-		} catch (e: Exception) {
-			channel.basicNack(envelope.deliveryTag, false, true) // Requeue on failure
-			logger.error("Error processing message: ${e.message}", e)
-		}
-	}
 }
