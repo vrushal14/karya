24398f814e4e149912fd56251a3da9e9c9e2838b
Saumya Bhatt <108668549+SaumyaBhatt106@users.noreply.github.com>
2024-12-04 17:42:06 +0530

    Connected dokka and configure GH actions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
   id(Plugins.Kotlin.JVM) version Plugins.Kotlin.VERSION apply false
   id(Plugins.Detekt.LIBRARY) version Plugins.Detekt.VERSION
+  id(Plugins.Dokka.LIBRARY) version Plugins.Dokka.VERSION
 }
 
 allprojects {
-
   apply(plugin = Plugins.Detekt.LIBRARY)
 
   repositories {
@@ -24,8 +24,10 @@ subprojects {
   version = "0.0.1"
 
   apply(plugin = Plugins.Kotlin.JVM)
+  apply(plugin = Plugins.Dokka.LIBRARY)
 
   dependencies {
     detektPlugins(Plugins.Detekt.FORMATTING)
   }
+
 }
--- buildSrc/src/main/kotlin/Plugins.kt
+++ buildSrc/src/main/kotlin/Plugins.kt
@@ -1,4 +1,5 @@
 object Plugins {
+
   object Kotlin {
     private const val KOTLIN_PREFIX = "org.jetbrains.kotlin"
     const val JVM = "$KOTLIN_PREFIX.jvm"
@@ -15,5 +16,10 @@ object Plugins {
     const val LIBRARY = "io.gitlab.arturbosch.detekt"
     const val VERSION = "1.23.4"
     const val FORMATTING = "$LIBRARY:detekt-formatting:$VERSION"
+  }
+
+  object Dokka {
+    const val LIBRARY = "org.jetbrains.dokka"
+    const val VERSION = "1.9.20"
   }
 }
